bac1c2419188b430041efb942bdd475134270ced
𝐄𝐦𝐞𝐫𝐬𝐨𝐧 𝐏𝐞𝐬𝐬𝐨𝐚 <60630092+emersonpessoa01@users.noreply.github.com>
2026-07-01 21:03:11 -0300

    :sparkles: feat: Completed the BrandsHighlight component with structure, styling, and schema

--- apps/brands-highlight/react/BrandsHighlight.tsx
+++ apps/brands-highlight/react/BrandsHighlight.tsx
@@ -1,15 +1,3 @@
-import React from "react";
-import { useCssHandles } from "vtex.css-handles";
-
-const CSS_HANDLES = ["container"] as const;
-
-type Props = {
-  name: string;
-};
-
-function BrandsHighlight({ name }: Props) {
-  const { handles } = useCssHandles(CSS_HANDLES);
-  return <div className={handles.container}>Hey, {name}</div>;
-}
+import BrandsHighlight from "./component/BrandsHightlight";
 
 export default BrandsHighlight;
