a2157cac060869b3dc98527376df477de6ad6974
𝐄𝐦𝐞𝐫𝐬𝐨𝐧 𝐏𝐞𝐬𝐬𝐨𝐚 <60630092+emersonpessoa01@users.noreply.github.com>
2026-07-04 20:13:24 -0300

    :sparkles: feat: Benefits bar block configured

--- apps/benefits-bar/react/component/BenefitsBar.tsx
+++ apps/benefits-bar/react/component/BenefitsBar.tsx
@@ -1,18 +1,41 @@
 import React from "react";
 import { useCssHandles } from "vtex.css-handles";
 
-const CSS_HANDLES = ["container"] as const;
+const CSS_HANDLES = [
+  "container",
+  "item",
+  "iconWrapper",
+  "icon",
+  "content",
+  "subtitle"
+] as const;
 
-type BenefitsBarProps = {
-  name: string;
+type Benefit={
+  title: string;
+  subtitle: string;
+  iconUrl: string;
+}
+
+interface BenefitsBarProps {
+  benefits: Benefit[];
 };
 
-function BenefitsBar({ name }: BenefitsBarProps) {
+function BenefitsBar({ benefits=[] }: BenefitsBarProps) {
   const { handles } = useCssHandles(CSS_HANDLES);
 
   return (
     <section className={handles.container}>
-      <h2>Benefits Bar {name}</h2>
+      {benefits.map(({title, subtitle, iconUrl}) => (
+        <div key={title} className={handles.item}>
+          <div className={handles.iconWrapper}>
+            <img src={iconUrl} alt={title} className={handles.icon} />
+          </div>
+          <div className={handles.content}>
+            <h3>{title}</h3>
+            <p className={handles.subtitle}>{subtitle}</p>
+          </div>
+        </div>
+      ))}
     </section>
   );
 }
